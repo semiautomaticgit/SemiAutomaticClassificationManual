e6d6795cc9dd17b88764ffeabae00f0bf1f5b2df
semiautomaticgit <semiautomatic.blogger@gmail.com>
2014-03-23 19:30:43 +0100

    new conf commit

--- conf.py
+++ conf.py
@@ -102,6 +102,11 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'basic'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'basic'
+else:
+    html_theme = 'basic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
